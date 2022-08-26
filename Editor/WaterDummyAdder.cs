0f06aae4fa40435eaa61c31c8db23420754f01ad
FranzFischer <f.franz.78@web.de>
2022-08-26 13:54:44 +0200

    some more stuff

---
+++ Editor/WaterDummyAdder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class WaterDummyAdder
+{
+	// Add menu named "My Window" to the Window menu
+	[MenuItem("Assets/Custom Islands/DummyWater")]
+	static void DummyWater()
+	{
+
+	}
+
+
+}
